dfcc3d4bc62bd25dfcb4f4cd4c63a30fcb3b8b50
Saajan <saajan.sn@gmail.com>
2020-04-06 22:40:12 +0530

    settings remove unused code

--- src/Screens/Settings/Settings.jsx
+++ src/Screens/Settings/Settings.jsx
@@ -1,74 +1,10 @@
-// import React, { useContext, useState, useEffect } from 'react';
-// import { Text, ScrollView, StyleSheet } from 'react-native';
-// import { Drawer } from 'native-base';
-// import { Center } from '../../Components/Center';
-// import { AuthContext } from "../../Context/AuthProvider";
-// const baseURL = 'https://nmsserver.herokuapp.com/proxy/api/user/company/master';
 
-// export default () => {
-//     const { logout } = useContext(AuthContext);
-//     const [loading, setLoading] = useState(false);
-//     const [data, setData] = useState([]);
-
-//     useEffect(() => {
-//         setLoading(true);
-//         fetch(baseURL, {
-//             headers: {
-//                 'Content-Type': 'application/json; charset=utf-8',
-//                 'token': '57Cz0gQ7tnq9jMTIW4WxlMPp4dxfA44ONRQTRo9G4Ok='
-//             },
-//             method: 'POST'
-//         })
-//             .then(response => response.json())
-//             .then(data => setData(data))
-//             .then((data) => {
-//                 setLoading(false);
-//                 console.log(data);
-//             })
-//             .catch(() => {
-//                 setLoading(false);
-//             })
-//     }, []);
-
-//     if (loading) {
-//         return (<Center>
-//             <Text>LOADING</Text>
-//         </Center>)
-//     }
-//     function getVal() {
-//         return (<Text>Display data!</Text>)
-//     }
-
-
-//     return (
-//         <ScrollView >
-
-//             <Center>
-//                 <Text style={styles.bigBlue}>Settings</Text>
-
-//             </Center>
-//             <Text>Displaying settings</Text>
-
-
-
-//         </ScrollView>
-
-//     )
-// }
-// const styles = StyleSheet.create({
-//     bigBlue: {
-//         color: 'blue',
-//         fontWeight: 'bold',
-//         fontSize: 30,
-//         margin: 20,
-//     }
-// });
-import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, View, Platform, Picker, ActivityIndicator, Button, Alert } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Picker, Button, Alert } from 'react-native';
 export default FunctionBasedForm = () => {
     const [PickerValueHolder, setPickerValueHolder] = useState('');
 
-    GetPickerSelectedItemValue = () => {
+    const GetPickerSelectedItemValue = () => {
         Alert.alert(PickerValueHolder);
     }
 
@@ -86,6 +22,7 @@ export default FunctionBasedForm = () => {
     );
 
 }
+
 const styles = StyleSheet.create({
     MainContainer: {
         justifyContent: 'center',
